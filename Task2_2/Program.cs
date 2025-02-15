339f9cf75b9fdb6a81042df7033237fca3a1b65c
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-16 00:46:05 +0300

    Обновил задание 2 и исправил задание 6

--- Task2_2/Program.cs
+++ Task2_2/Program.cs
@@ -12,14 +12,17 @@
             Console.Write("Секунды=");
             int c = Convert.ToInt32(Console.ReadLine());
 
+
             double r1 = (double)a * Math.PI / 180.0;
-            double r2 = (double)b * Math.PI / 60.0/ 180.0;
+            double r2 = (double)b * Math.PI / 60.0 / 180.0;
             double r3 = (double)c * Math.PI / 60.0 / 60.0 / 180.0;
+            double r4 = (double)(a - a / 360 * 360) * Math.PI / 180.0;
 
+            double R1 = r1 + r2 + r3;
+            double R2 = r4 + r2 + r3;
 
-            double R = r1 + r2 + r3;
-
-            Console.WriteLine("Значение в радианах ={0}", R);
+            Console.WriteLine("Значение в радианах ={0}", R1);
+            Console.WriteLine("Значение в радианах минимальное ={0}", R2);
             Console.WriteLine();
         }
     }
--- Task2_6/Program.cs
+++ Task2_6/Program.cs
@@ -8,15 +8,13 @@
 
             double L = Convert.ToDouble(Console.ReadLine());
 
-            double t1 = L % 1*4;
-            double t2 = L % 1*2;
+            double t1 = L % 1 * 100 / 25;
+            double t2 = L % 1 * 100 / 50;
 
-            double l = Math.Floor(L) + Math.Floor(t1)*0.5 - Math.Floor(t2) * 0.5;
+            double l = Math.Floor(L) + Math.Floor(t1) * 0.5 - Math.Floor(t2) * 0.5;
 
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
             Console.Write("Округление до 0.5: {0}", l);
             Console.WriteLine();
-         }
+        }
     }
 }
