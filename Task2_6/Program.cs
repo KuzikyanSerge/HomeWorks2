e67a4007ad120393f4704e52cff78f3bdeea9696
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-18 01:13:40 +0300

    Добавил альтернативные варианты реализации

--- Task2_6/Program.cs
+++ Task2_6/Program.cs
@@ -13,6 +13,15 @@
 
             double l = Math.Floor(L) + Math.Floor(t1) * 0.5 - Math.Floor(t2) * 0.5;
 
+            //Альтернативный вариант 1. В "b" вписать число до которого нужно округлить:
+            //const double b = 0.5;
+            //double l = Math.Round((L - Math.Floor(L)) * 1 / b) * b + Math.Floor(L);
+
+            //Альтернативный вариант 2. Можно указать число до которого нужно округлить в консоли: 
+            //Console.WriteLine("Введите число до которого необходимо округлить в формате 0,01:");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //double l = Math.Round((L - Math.Floor(L)) * 1 / b) * b + Math.Floor(L);
+
             Console.Write("Округление до 0.5: {0}", l);
             Console.ReadKey();
         }
