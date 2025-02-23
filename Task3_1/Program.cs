83f1f4f3a865f5b3d9f631fa69c675187c2f0eb8
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-23 19:57:18 +0300

    Решение первой задачи

---
+++ Task3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите первое целое число:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе целое число:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            string simbol = (firstNumber > secondNumber) ? ">" : ((firstNumber < secondNumber) ? "<" : "=");
+            Console.WriteLine("Сравнение {0} {1} {2}", firstNumber, simbol, secondNumber);
+            Console.ReadKey();
+        }
+    }
+}
