713a00ec1bb7328a25d8b03f338df9ec3e7b2794
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-23 22:17:26 +0300

    добавил 4 задание

---
+++ Task3_4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите первое целое число:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе целое число:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите третье целое число:");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            int max1 = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+            int max = (max1 > thirdNumber) ? max1 : thirdNumber;
+
+            int min1 = (firstNumber < secondNumber) ? firstNumber : secondNumber;
+            int min = (min1 < thirdNumber) ? min1 : thirdNumber;
+
+            int mediana = ((firstNumber != max) && (firstNumber != min)) ? firstNumber : (((secondNumber != max) && (secondNumber != min)) ? secondNumber : thirdNumber);
+
+            Console.WriteLine("Медиана {0}", mediana);
+            Console.ReadKey();
+        }
+    }
+}
