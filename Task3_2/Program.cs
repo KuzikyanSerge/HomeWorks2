0be80ddf856c01aedd9e600ec6789405703c0027
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-23 20:38:21 +0300

    Добавил ренешия по 3 и 4 задачам

---
+++ Task3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите первое целое число:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе целое число:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите третье целое число:");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            int max = ((firstNumber > secondNumber) && (firstNumber > thirdNumber)) ? firstNumber : ((secondNumber > thirdNumber) ? secondNumber : thirdNumber);
+
+            Console.WriteLine("Самое большое число: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите первое целое число:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе целое число:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите третье целое число:");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите четвертое целое число:");
+            int fourthNumber = Convert.ToInt32(Console.ReadLine());
+
+            int max1 = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+            int max1 = (thirdNumber > fourthNumber) ? thirdNumber : fourthNumber;
+            int max = (max1 > max2) ? max1 : max2;
+
+
+            Console.WriteLine("Самое большое число: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
