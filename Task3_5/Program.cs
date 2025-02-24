691e352efe59030aeabaaba1c745755a72918ea5
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-02-24 16:25:23 +0300

    добавил задание 5

---
+++ Task3_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string answer = (number % 10 == 0) ? "Число оканчивается на 0" : "Число НЕ оканчивается на 0";
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+            //Альтернативный вариант
+            //if (number % 10 == 0)
+            //{
+            //    Console.WriteLine("Число оканчивается на 0");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число НЕ оканчивается на 0");
+            //    Console.ReadKey();
+            //}
+
+
+        }
+    }
+}
