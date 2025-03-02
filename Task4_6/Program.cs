18f04fa73ec2066d904646ff1a77a0990df21bfe
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-03-02 17:04:20 +0300

    Добавил 6 задание

---
+++ Task4_6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task4_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int n = 1;
+            int s = 0;
+            while (number != n)
+            {                                 
+                    n *= 2;
+                    s++;
+                    if (number < n)
+                    {
+                        Console.WriteLine("Число нельзя представить как степень 2-ки");
+                        break; 
+                    }                
+            }
+            if (number == n) 
+                Console.WriteLine("Число можно представить как 2^{0}", s);
+            Console.ReadKey();
+        }
+    }
+}
