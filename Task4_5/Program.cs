0deb64a5e9b8b3c8dc6355bdd427bff75c356217
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-03-02 16:17:51 +0300

    Добавил 5 задание

---
+++ Task4_5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            if ((number < 20) || (number > 60))
+            {
+                Console.WriteLine("Введите другое число:");
+                while ((number < 20) || (number > 60))
+                {
+                    double n = Convert.ToDouble(Console.ReadLine());
+                    if ((n < 20) || (n > 60))
+                    {
+                        Console.WriteLine("Введите другое число:");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Число входит в диапазон [20; 60]");
+            Console.ReadKey();
+        }
+    }
+}
