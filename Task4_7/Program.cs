19180370b36de3f193f48ed3bc8fa6c1c6c82653
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-03-02 17:49:05 +0300

    Добавил 7 задание

---
+++ Task4_7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int n = 1;
+            int s = 0;
+            while (number / n > 0)
+            {                
+                    n *= 10;
+                    s++;                
+            }
+            Console.WriteLine("Количество цифр в числе = {0}", s);
+            Console.ReadKey();
+        }
+    }
+}
