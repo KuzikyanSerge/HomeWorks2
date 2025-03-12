4b0bd5b168b7f4f6aaf91b5b0cfcf41e53ca96cc
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-03-13 00:20:22 +0300

    fix

--- Task3_11/Program.cs
+++ Task3_11/Program.cs
@@ -24,25 +24,25 @@ namespace Task3_11
             Console.Write("s=");
             int s = Convert.ToInt32(Console.ReadLine());
 
-            //string answer = 
-            //    ((p + r <= a) && q <= b && s <= b)
-            //    ||
-            //    (p <= a && r <= a && (q + s <= b))
-            //    ||
-            //    ((p + s <= a) && q <= b && r <= b)
-            //    ||
-            //    (p <= a && s <= a && (q + r <= b))
-            //    ||
-            //    ((p + r <= b) && q <= a && s <= a)
-            //    ||
-            //    (p <= b && r <= b && (q + s <= a))
-            //    ||
-            //    ((p + s <= b) && q <= a && r <= a)
-            //    ||
-            //    (p <= b && s <= b && (q + r <= a))
-            //    ? "Можно разместить" : "Невозможно разместить";
+            string answer =
+                ((p + r <= a) && q <= b && s <= b)
+                ||
+                (p <= a && r <= a && (q + s <= b))
+                ||
+                ((p + s <= a) && q <= b && r <= b)
+                ||
+                (p <= a && s <= a && (q + r <= b))
+                ||
+                ((p + r <= b) && q <= a && s <= a)
+                ||
+                (p <= b && r <= b && (q + s <= a))
+                ||
+                ((p + s <= b) && q <= a && r <= a)
+                ||
+                (p <= b && s <= b && (q + r <= a))
+                ? "Можно разместить" : "Невозможно разместить";
 
-            string answer = (p * q + s * r <= a * b) && ((p <= a && r <= a && q <= a && s <= a) || (q <= b && s <= b && p <= b && r <= b)) ? "Можно разместить" : "Невозможно разместить";
+            //string answer = (p * q + s * r <= a * b) && ((p <= a && r <= a && q <= a && s <= a) || (q <= b && s <= b && p <= b && r <= b)) ? "Можно разместить" : "Невозможно разместить";
 
             Console.WriteLine(answer);
             Console.ReadKey();
