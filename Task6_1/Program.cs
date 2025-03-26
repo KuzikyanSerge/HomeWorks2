3b00ad0d3e552349eba943d1e8f03f487ac1d738
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-03-27 00:18:09 +0300

    Добавил задание 1

---
+++ Task6_1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод данных
+            Console.Write("Введите строку из слов, разделенных пробелами: ");
+            string input = Console.ReadLine();
+
+            //Избавляемся от лишних пробелов
+            while (input.Contains("  "))
+            {
+                input = input.Replace("  ", " ");
+            }
+            input = input.Trim();
+
+            //Разделяем строку на слова
+            string[] words = input.Split(' ');
+                        
+            // Обрабатываем каждое слово
+            int l = words[0].Length;
+            string s = words[0];
+            foreach (string word in words)
+            {
+                if (word.Length > l)
+                {
+                    l = word.Length;
+                    s = word;
+                }
+            }    
+
+            //Вывод результата
+            Console.WriteLine("Самоое длинное слово:");
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
