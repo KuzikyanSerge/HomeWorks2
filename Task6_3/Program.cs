5fd2eb0881a8158884284c9c489c357dba3a0d9c
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-04-22 19:02:52 +0300

    добавл 3 задание

---
+++ Task6_3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод данных
+            Console.Write("Введите месяц: ");
+            string month = Console.ReadLine();
+            Console.Write("Введите год: ");
+            string year = Console.ReadLine();
+            Console.Write("Общую сумму продаж: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Количество проданных товаров: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            //Расчет средней стоимости товара
+            double price = sum / quantity;
+
+            //Вывод отчета(решил попроблвать с переносом строки одной формой)
+            //В задании написано что только у суммы нужно тысячи отделять, но в примере во всех значениях тысячи отделены, сделал по заданию чтобы разные форматы были
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Отчёт о продажах за {0} {1}:\n\nОбщая сумма продаж: {2:N2} р.\nКоличество проданных товаров: {3} шт.\nСредняя стоимость товара: {4:N2} р.", month, year, sum, quantity, price);
+            Console.WriteLine("--------------------");
+            Console.ReadKey();
+        }
+    }
+}
