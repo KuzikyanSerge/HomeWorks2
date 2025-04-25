c6d79e10cb133a9c2257d8c725aa965df88112ac
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-04-25 13:34:22 +0300

    Добавил 4 задание

---
+++ Task7_4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.ReadKey();
+        }
+
+        static int FindMax(params int[] numbers)
+        {
+            int maxNumber = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                    maxNumber = numbers[i];
+            }
+            return maxNumber; 
+        }
+    }
+}
