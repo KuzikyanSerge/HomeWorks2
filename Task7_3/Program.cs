2efcee97dce6c1239bdac08d7b04ecfb4baa7d9a
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-04-25 13:27:16 +0300

    Добавил 3 задание

---
+++ Task7_3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            PrintNumbers(numbers);
+            PrintNumbers(numbers, true);
+            Console.ReadKey();
+        }
+
+        static void PrintNumbers(int[] numbers, bool reverse = false)
+        {
+            if (reverse == false)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write("{0} ", numbers[i]);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = numbers.Length - 1; i >= 0; i--)
+                {
+                    Console.Write("{0} ", numbers[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
