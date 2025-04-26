6ab7ef2bb1063a9339eb1de1eaf5c9a55d2ebe9c
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-04-26 18:13:04 +0300

    Добавил задание 9.1

---
+++ Task9_1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Task9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Война и мир", "Л.Н.Толстой", 1869, 1225);
+            book.GetInfo();
+            // Попытка задать недопустимый год
+            // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
+            // Если это надо было сделать то можно дополнить код тем что закомментировано
+            //try
+            //{
+            //    book.Year = -1869;
+            //    book.Pages = -1225;
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine($"Ошибка: {e.Message}");
+            //}
+            //book.GetInfo();
+            Console.ReadKey();
+        }
+    }
+    public class Book
+    {
+        //Поля
+        private string _title;
+        private string _author;
+        // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
+        // Если это надо было сделать, то можно дополнить код тем что закомментировано
+        //private int _year;
+        //private int _pages;
+
+        //Автосвойства
+        public int Year { get; set; }
+        public int Pages { get; set; }
+        // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
+        // Если это надо было сделать, то можно дополнить код тем что закомментировано
+        //public int Year 
+        //{ 
+        //    get => _year; 
+            
+        //    set
+        //    {
+        //        if (value <= 0)
+        //            throw new ArgumentException("Год не может быть отрицательным или нулевым!");
+        //        _year = value;
+        //    }
+        //}        
+        
+        //public int Pages
+        //{
+        //    get => _pages;
+
+        //    set
+        //    {
+        //        if (value <= 0)
+        //            throw new ArgumentException("Количество страниц не может быть отрицательным или нулевым!");
+        //        _pages = value;
+        //    }
+        //}
+
+        //Конструктор
+        public Book(string title, string author, int year, int pages)
+        {
+            _title = title;
+            _author = author;
+            Year = year;
+            Pages = pages;
+
+        }
+        //Метод
+        public void GetInfo()
+        {
+            Console.WriteLine($"\"{_title}\", {_author}, {Year}г., {Pages} стр.");
+        }
+
+    }
+}
