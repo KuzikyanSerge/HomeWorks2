e4ebbb3f849c65349222080b02efb653410b2a1c
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-04-28 01:57:55 +0300

    Исправил задание 9.1

--- Task9_1/Program.cs
+++ Task9_1/Program.cs
@@ -5,20 +5,16 @@
         static void Main(string[] args)
         {
             Book book = new Book("Война и мир", "Л.Н.Толстой", 1869, 1225);
+            try
+            {
+                book.Year = -1869;
+                book.Pages = -1225;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
             book.GetInfo();
-            // Попытка задать недопустимый год
-            // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
-            // Если это надо было сделать то можно дополнить код тем что закомментировано
-            //try
-            //{
-            //    book.Year = -1869;
-            //    book.Pages = -1225;
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine($"Ошибка: {e.Message}");
-            //}
-            //book.GetInfo();
             Console.ReadKey();
         }
     }
@@ -27,39 +23,33 @@
         //Поля
         private string _title;
         private string _author;
-        // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
-        // Если это надо было сделать, то можно дополнить код тем что закомментировано
-        //private int _year;
-        //private int _pages;
+        private int _year;
+        private int _pages;
 
-        //Автосвойства
-        public int Year { get; set; }
-        public int Pages { get; set; }
-        // В задании указано что год и количество страниц должны быть автосвойствами, а не проверяемыми
-        // Если это надо было сделать, то можно дополнить код тем что закомментировано
-        //public int Year 
-        //{ 
-        //    get => _year; 
-            
-        //    set
-        //    {
-        //        if (value <= 0)
-        //            throw new ArgumentException("Год не может быть отрицательным или нулевым!");
-        //        _year = value;
-        //    }
-        //}        
-        
-        //public int Pages
-        //{
-        //    get => _pages;
+        //Автосвойства или полное свойство с проверкой
+        public int Year
+        {
+            get => _year;
 
-        //    set
-        //    {
-        //        if (value <= 0)
-        //            throw new ArgumentException("Количество страниц не может быть отрицательным или нулевым!");
-        //        _pages = value;
-        //    }
-        //}
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Год не может быть отрицательным или нулевым!");
+                _year = value;
+            }
+        }
+
+        public int Pages
+        {
+            get => _pages;
+
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Количество страниц не может быть отрицательным или нулевым!");
+                _pages = value;
+            }
+        }
 
         //Конструктор
         public Book(string title, string author, int year, int pages)
